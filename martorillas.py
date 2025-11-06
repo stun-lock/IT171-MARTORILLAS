93122c0bc659285c65bd2782a18f0b650e1caba0
stun-lock <6marklouie9@gmail.com>
2025-11-07 01:47:01 +0800

    Update martorillas.py
    Added player movement

--- martorillas.py
+++ martorillas.py
@@ -1,11 +1,17 @@
-player_x = 0
-player_y = 0
-treasure_x = 5
+player_mark = 0
+player_louie = 0
+treasure_x = 19
 treasure_y = 3
 game_running = True
 print(f"Find the treasure at ({treasure_x}, {treasure_y})!")
 while game_running:
-move = input("Enter move (up/down/left/right): ")
-# TODO: update player_x and player_y based on move
-print(f"Player position: ({player_x}, {player_y})")
-# TODO: check if player has reached the treasure
+  move = input("Enter move (up/down/left/right): ")
+    if move == "right":
+        player_mark += 1 
+    if move == "left":
+        player_mark -= 1
+    if move == "up":
+        player_louie += 1
+    if move == "down":
+        player_louie -= 1
+    print(f"Player position: ({player_mark}, {player_louie})")
