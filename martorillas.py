efa960778a0bb7fffd85222df3d22a517a9231be
stun-lock <6marklouie9@gmail.com>
2025-11-07 02:11:41 +0800

    Update martorillas.py (crucial)
    Realized that the spacing for most of the code was messed up, had to make it consistent.

--- martorillas.py
+++ martorillas.py
@@ -6,17 +6,17 @@ game_running = True
 print(f"Find the treasure at ({treasure_x}, {treasure_y})!")
 while game_running:
   move = input("Enter move (up/down/left/right): ")
-    if move == "right":
-        player_mark += 1 
-    if move == "left":
-        player_mark -= 1
-    if move == "up":
-        player_louie += 1
-    if move == "down":
-        player_louie -= 1
-    print(f"Player position: ({player_mark}, {player_louie})")
-   if player_mark == treasure_x and player_louie == treasure_y:
-        print("win")
-        break
-    if move == "q":
-        break
+  if move == "right":
+    player_mark += 1 
+  if move == "left":
+    player_mark -= 1
+  if move == "up":
+    player_louie += 1
+  if move == "down":
+      player_louie -= 1
+  print(f"Player position: ({player_mark}, {player_louie})")
+  if player_mark == treasure_x and player_louie == treasure_y:
+    print("win")
+    break
+  if move == "q":
+    break
