6f4f722a567d87c201bc3b996c5d8998e5b765f0
stun-lock <6marklouie9@gmail.com>
2025-11-10 21:20:44 +0800

    Added instructions to play the game (as well as changed the win and exit message a bit)

--- martorillas.py
+++ martorillas.py
@@ -3,7 +3,17 @@ player_louie = 0
 treasure_x = 19
 treasure_y = 3
 game_running = True
-print("Welcome to Martorillas’ Maze")
+print("""
+----------------------------
+Welcome to Martorillas’ Maze
+----------------------------
+""")
+print("""You have 4 options of movement.
+Type 'up' to move upwards (+1 y-level)
+Type 'down' to move downwards (-1 y-level)
+Type 'right' to move to the right (+1 x-level)
+Type 'left' to move to the left (-1 x-level)
+""")
 print(f"Find the treasure at ({treasure_x}, {treasure_y})!")
 while game_running:
   move = input("Enter move (up/down/left/right): ")
@@ -17,7 +27,8 @@ while game_running:
       player_louie -= 1
   print(f"Player position: ({player_mark}, {player_louie})")
   if player_mark == treasure_x and player_louie == treasure_y:
-    print("win")
+    print("You've won the game!")
     break
   if move == "q":
+    print("Game has been stopped.")
     break
